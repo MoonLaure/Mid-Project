b5e8e42878030ccb34830e7ebf614eb7247c640c
MoonLaure <moon20059420@gmail.com>
2026-05-01 23:56:47 +0800

    Delete auth.js

--- auth.js
+++
@@ -1,8 +0,0 @@
-function login(){
-  const role = document.getElementById("role").value;
-  localStorage.setItem("role", role);
-
-  if(role==="waiter") location.href="order.html";
-  if(role==="kitchen") location.href="kitchen.html";
-  if(role==="manager") location.href="inventory.html";
-}
